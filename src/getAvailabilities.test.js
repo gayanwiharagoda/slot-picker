d1f7c9cef769962f5af419b237698dfa01a4379e
gayanwiharagoda <gayanwiharagoda@gmail.com>
2019-10-14 21:28:50 +0700

    [fix] test case and adding by order

--- src/getAvailabilities.test.js
+++ src/getAvailabilities.test.js
@@ -31,7 +31,7 @@ describe("getAvailabilities", () => {
       ]);
     });
 
-    it.only("test 1", async () => {
+    it("test 1", async () => {
       const availabilities = await getAvailabilities(new Date("2014-08-10"));
       expect(availabilities.length).toBe(7);
 
@@ -47,7 +47,7 @@ describe("getAvailabilities", () => {
         "9:30",
         "10:00",
         "11:30",
-        "14:00"
+        "12:00"
       ]);
 
       expect(String(availabilities[6].date)).toBe(
@@ -85,7 +85,14 @@ describe("getAvailabilities", () => {
       expect(String(availabilities[1].date)).toBe(
         String(new Date("2014-08-11"))
       );
-      expect(availabilities[6].slots).toEqual([]);
+      expect(availabilities[6].slots).toEqual([
+        "9:30",
+        "10:00",
+        "10:30",
+        "11:00",
+        "11:30",
+        "12:00"
+      ]);
     });
   });
 });
